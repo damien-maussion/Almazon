c37365100bf5aa172662703d607983b2ec6c4287
yNoblet <E104834A@I011V7pc06.ensinfo.sciences.univ-nantes.prive>
2015-11-18 16:14:53 +0100

    test fonctionnel

---
+++ Client/src/main/webapp/almazon.js
@@ -0,0 +1,42 @@
+function test(){
+	alert("Hello World!");
+	$.soap({
+    	url: 'http://localhost:9763/services/ShopService/',
+    	namespaceURL:'http://ShopService.shop.univnantes.fr'
+	});
+
+	$.soap({
+		method: 'getExchangeRate',
+		data: {},
+		soap12: true,
+		success: function (soapResponse) {
+		    // do stuff with soapResponse
+		    console.log(soapResponse);
+		    //console.log(soapResponse.toString());
+		    //alert(soapResponse.toString());
+		    updateProducts(soapResponse.toJSON());
+		},
+		error: function (soapResponse) {
+		    console.log('that other server might be down...');
+		    console.log(soapResponse);
+		    console.log(soapResponse.toString());
+		}
+	});
+}
+
+var cleanProducts = function(o)
+{
+	return o['#document']['ns:getExchangeRateResponse']['ns:return'];
+};
+
+var updateProducts = function(products)
+{
+	products=cleanProducts(products);
+	console.log(products);
+	alert(products);
+	//$('.products').html("<ul></ul>");
+	/*for(var i =0; i<products.length;i++)
+	{
+		$('.products ul').append().append('<li><div>'+products[i].name["_"]+'</div><div>Quantity: '+products[i].quantity["_"]+'</div><div>Price: '+products[i].price["_"]+'</div><div><button onclick="add(\''+products[i].name["_"]+'\')">Ajouter</button><div/></li>');
+	}*/
+};
--- Client/src/main/webapp/index.jsp
+++ Client/src/main/webapp/index.jsp
@@ -1,7 +1,17 @@
 <html>
-<body>
-<h2>Hello World!</h2>
-</body>
-<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
-<script type="text/javascript" src="bower_components/jquery.soap/jquery.soap.js"></script>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="styles/style_almazon.css" />
+		<title>Almazon</title>
+	</head>
+	
+	<body>
+
+		<h2 onclick="test()">Hello World!</h2>
+		<button onclick="test()">test</button>
+		<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+		<script type="text/javascript" src="bower_components/jquery.xml2json/src/xml2json.js"></script>
+		<script type="text/javascript" src="bower_components/jquery.soap/jquery.soap.js"></script>
+		<script type="text/javascript" src="almazon.js"></script>
+	</body>
 </html>
